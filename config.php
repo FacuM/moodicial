27ab9c1ad3954b30188140d3d181db9396ce5ba8
FacuM <facumo.fm@gmail.com>
2018-02-23 14:48:16 -0300

    Comment out most of the code and how it is supposed to behave.

--- config.php
+++ config.php
@@ -1,23 +1,28 @@
 <?php
- // Website information
+ // This array saves the website information
  $info = array (
   'title'			=> 'Moodicial'
  );
  
+ // This array saves the website credentials.
  $credentials = array (
-  'hostname'  	    => '127.0.0.1',
-  'port' 	        => '3306',
-  'username' 	    => 'root',
-  'password' 	    => '',
-  'db' 		        => 'moodicial',
-  'ptable' 	        => 'moodicial_posts',
+  'hostname'		=> '127.0.0.1',
+  'port'				   => '3306',
+  'username'		=> 'root',
+  'password'		=> '',
+  'db'					  => 'moodicial',
+  'ptable' 	          => 'moodicial_posts',
  );
  
+ // Max amount of reports
  $maxrep = 5;
+ // Server document root. Set this to the path to your website files, omitting the root dir. If you put them in '/var/www/moodicial' write just '/moodicial'.
  $path= '/moodicial';
  
  // From now on, don't edit anything as you could break the whole website.
  
+ // Create the PDO objet for the MySQL server connection.
  $server = new PDO('mysql:host=' . $credentials['hostname'] . ':' . $credentials['port'] . ';dbname=' . $credentials['db'] . ';charset=utf8', $credentials['username'], $credentials['password']);
+ // Set the URL that would point to the root of the server and hold the index.
  $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . "://" . $_SERVER['HTTP_HOST'] . $path;
 ?>

5099d39d67e5fb336050c5a13852213dea6e3d61
FacuM <facumo.fm@gmail.com>
2018-02-23 13:12:40 -0300

    modularize top part of the website

---
+++ beginning.php
@@ -0,0 +1,8 @@
+<?php
+ echo "
+ <body>
+  <div class='page-header'>
+    <a href='$root'><center><h1>$info[title]</h1></center></a>
+  </div>
+  ";
+?>
---
+++ head.php
@@ -0,0 +1,13 @@
+<?php
+ echo "<html>
+ <head>
+  <title>$info[title]</title>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+  <link rel='stylesheet' href='https://bootswatch.com/4/cyborg/bootstrap.min.css'>
+  <link rel='stylesheet' href='mod.css'>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/octicons/4.4.0/font/octicons.min.css'
+ </head>
+ ";
+?>

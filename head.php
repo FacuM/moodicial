23ae747f1d2781e5015bfd65c612956f54e0e456
Facundo Montero <facumo.fm@gmail.com>
2018-02-25 21:18:48 -0300

    add favicons

--- head.php
+++ head.php
@@ -4,6 +4,13 @@
   <title>$info[title]</title>
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel='apple-touch-icon' sizes='76x76' href='/resources/favicons/apple-touch-icon.png'>
+  <link rel='icon' type='image/png' sizes='32x32' href='/resources/favicons/favicon-32x32.png'>
+  <link rel='icon' type='image/png' sizes='16x16' href='/resources/favicons/favicon-16x16.png'>
+  <link rel='manifest' href='/resources/favicons/site.webmanifest'>
+  <link rel='mask-icon' href='/resources/favicons/safari-pinned-tab.svg' color='#5bbad5'>
+  <meta name='msapplication-TileColor' content='#da532c'>
+  <meta name='theme-color' content='#ffffff'>
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
   <link rel='stylesheet' href='https://bootswatch.com/4/cyborg/bootstrap.min.css'>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/octicons/4.4.0/font/octicons.min.css'>

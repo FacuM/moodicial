9ecad3855b1487a9bb64c710537d38300e8d5483
Facundo Montero <facumo.fm@gmail.com>
2018-02-28 00:08:23 -0300

    add Portuguese (BR) support

---
+++ lang/pt_BR.php
@@ -0,0 +1,63 @@
+<?php
+ // This is the default language, it's a PHP file that contains an array with all the required strings for Moodicial to work.
+ // Filename: en_US.php
+ // Author: Facundo Montero <facumo.fm@gmail.com>
+
+ $LANG = array (
+  // Home
+   // Empty website
+   'no_data_a'                => 'Ei! Parece que ninguém publicou nada ainda, você gostaria ',
+   'no_data_b'                => 'ser o primeiro?',
+   // Reporting
+   'report_err_disabled'      => 'O sistema de queixas foi desativado pelo proprietário deste site.',
+   'report_button'            => 'Denunciar',
+   // Posts
+   'posted_by'                => 'Publicado por ',
+   // Commenting
+   'comment_button_create'    => 'Comente',
+   'comment_after_nick'       => 'disse...',
+   // Images
+   'alt_broken_image'         => 'Imagem',
+   // Content loading status
+   'is_loading'               => 'Carregando...',
+   'is_lastpage_a'            => 'Não há mais conteúdo para mostrar, você gostaria ',
+   'is_lastpage_b'            => ' atualizar?',
+   'is_err'                   => 'Não foi possível carregar os dados, por favor entre em contato com o administrador!',
+  // Metrics
+   // Visits
+   'visits'                   => 'Visitas',
+  // Forms
+   // Comments
+   'comment_content_label'    => 'Conteúdo do comentário',
+   // Posts
+   'post_content_label'       => 'Conteúdo da publicação',
+   // Both
+   'nick_label'               => 'Nick (opcional)',
+   'image_url_label'          => 'URL da imagem (opcional)',
+   'forms_button_submit'      => 'Enviar',
+  // Footer
+  'footer_credits_a'          => 'Funciona graças a Moodicial, escrito por Facundo Montero usando Bootstrap. Dê uma olhada no ',
+  'footer_credits_insource'   => 'código fonte deste arquivo',
+  'footer_credits_b'          => ' ou visite o ',
+  'footer_credits_exsource'   => 'repositório completo',
+  // Installer
+  'installer_no_db'           => 'Ei! Parece que você esqueceu de criar completamente o banco de dados antes de tentar instalar seu site. Mas não se sinta mal, eu fiz isso por você!',
+  'installer_db_creation_ok'  => 'Sucesso criando o banco de dados ',
+  'installer_db_connecting'   => 'Conectando ao servidor do banco de dados ...',
+  'installer_db_connect_ok'   => 'Feito. Conexão bem-sucedida!',
+  'installer_db_err'          => 'Algo deu errado, verifique sua configuração em ',
+  'installer_db_creating'     => 'Criando o banco de dados...',
+  'installer_db_c_ctable'     => 'Criando a tabela de comentários...',
+  'installer_db_c_ptable'     => 'Criando a tabela de publicações...',
+  'installer_db_c_mtable'     => 'Criando a tabela de estatísticas...',
+  'installer_db_c_table_ok'   => 'Sucesso criando a tabela ',
+  'installer_metrics_visits'  => 'Criando a entrada que irá armazenar as visitas em ',
+  'installer_metrics_ok'      => 'Sucesso inicializando o banco de dados de estatísticas.',
+  'installer_success_a'       => 'Muito bom trabalho! Parece que tudo correu bem;). Você gostaria ',
+  'installer_success_b'       => 'tentar seu site agora?',
+  'installer_metrics_cleanup' => 'Todas as entradas foram removidas de ',
+  // All
+  'no_nick'                   => 'Anônimo'
+ );
+
+?>

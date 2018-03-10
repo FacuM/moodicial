0dde8e404fda2bf207b4ba8e4ef51a2598160fb9
FacuM <facumo.fm@gmail.com>
2018-03-09 21:34:15 -0300

    indentation

--- comment.php
+++ comment.php
@@ -8,46 +8,46 @@ if (isset($_SESSION['lastinteraction']) && $time < $throttletime / 1000)
 }
 else
 {
- require_once("beginning.php");
- if (isset($_POST['pid']))
- {
-  $pid = $server->quote($_POST['pid']);
- }
- if (isset($_POST['content']))
- {
-  $_SESSION['lastinteraction'] = microtime(true);
-  if (empty($_POST['content']))
+  require_once("beginning.php");
+  if (isset($_POST['pid']))
   {
-   echo "
-    <script type='text/javascript'>
-     window.location = '$root';
-    </script>";
+    $pid = $server->quote($_POST['pid']);
+  }
+  if (isset($_POST['content']))
+  {
+    $_SESSION['lastinteraction'] = microtime(true);
+    if (empty($_POST['content']))
+    {
+      echo "
+      <script type='text/javascript'>
+      window.location = '$root';
+      </script>";
+    }
+    else
+    {
+      $content = strip_tags($_POST['content']);
+      if (isset($_POST['nick']))
+      {
+        $nick = $server->quote($_POST['nick']);
+      }
+      else
+      {
+        $nick = '';
+      }
+      $server->query("INSERT INTO `" . $credentials['ctable'] . "` (`nick`, `date`, `pid`, `cont`, `img`) VALUES (" . $nick . ", now(), " . $pid . ", " . $server->quote($content) . ", " . (!@getimagesize($server->quote($_POST['image'])) ? "''" : $server->quote($_POST['image'])) . ")");
+      echo "
+      <script type='text/javascript'>
+      window.location = '$root';
+      </script>"
+      ;
+    }
   }
   else
   {
-   $content = strip_tags($_POST['content']);
-   if (isset($_POST['nick']))
-   {
-    $nick = $server->quote($_POST['nick']);
-   }
-   else
-   {
-    $nick = '';
-   }
-   $server->query("INSERT INTO `" . $credentials['ctable'] . "` (`nick`, `date`, `pid`, `cont`, `img`) VALUES (" . $nick . ", now(), " . $pid . ", " . $server->quote($content) . ", " . (!@getimagesize($server->quote($_POST['image'])) ? "''" : $server->quote($_POST['image'])) . ")");
-   echo "
-    <script type='text/javascript'>
-     window.location = '$root';
-    </script>"
-   ;
+    echo "<script>
+    window.location = '$root';
+    </script>";
   }
- }
- else
- {
-  echo "<script>
-   window.location = '$root';
-  </script>";
- }
- require_once("footer.php");
+  require_once("footer.php");
 }
 ?>

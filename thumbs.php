2c4a89db027e3dfe935cd592fbb11cbf55f9d957
FacuM <facumo.fm@gmail.com>
2018-03-11 21:46:03 -0300

    add reactions processing script

--- thumbs.php
+++ thumbs.php
@@ -1,14 +1,25 @@
 <?php
 require_once("config.php");
 session_start();
-$time = microtime(true) -  $_SESSION['lastinteraction'];
+if (isset($_SESSION['lastinteraction']))
+{
+ $time = microtime(true) -  $_SESSION['lastinteraction'];
+}
 if (isset($_SESSION['lastinteraction']) && $time < $throttletime / 1000)
 {
   die('Limited');
 }
 else
 {
- $server->query("UPDATE `" . $credentials['ptable'] . "` SET " . (isset($_POST['action']) && $_POST['action'] ? '`up`=up+1' : '`down`=down+1') . "WHERE pid = " . $server->quote($_POST['pid']));
- echo $server->query("SELECT * FROM " . $credentials['ptable'] . " WHERE pid = " . $server->quote($_POST['pid']))->fetch[($_POST['action'] ? 'up' : 'down')];
+ if (isset($_POST['action']) && isset($_POST['pid']) && $thumbsmod)
+ {
+  $_SESSION['lastinteraction'] = microtime(true);
+  $server->query("UPDATE `" . $credentials['ptable'] . "` SET " . ($_POST['action'] === 'false' ? '`down`=down+1' : '`up`=up+1') . " WHERE pid = " . $server->quote($_POST['pid']));
+  echo $server->query("SELECT * FROM " . $credentials['ptable'] . " WHERE pid = " . $server->quote($_POST['pid']))->fetch()[($_POST['action'] === 'false' ? 'down' : 'up')];
+ }
+ else
+ {
+  header('location: ' . $root);
+ }
 }
 ?>

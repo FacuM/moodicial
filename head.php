4e580f291062a302876ddb7e6f651d2a30f1b5b8
FacuM <facumo.fm@gmail.com>
2018-03-11 21:34:51 -0300

    performance improvements (remove unneded function, use ternary operator directly), trigger cache updates

--- head.php
+++ head.php
@@ -8,19 +8,6 @@
  {
   require_once("lang/en_US.php");
  }
- // Detect required third part content availability.
- function chooseloc($url,$nointernet)
- {
-  if (!$nointernet)
-  {
-   $GLOBALS['final'] = $url;
-   return true;
-  }
-  else
-  {
-   return false;
-  }
- }
  // If the whole page is requested ($noformat = false), use ternary operators and the previously defined 'chooseloc' function to detect whether CDNs are reachable or not.
  if (!$noformat)
  {
@@ -36,24 +23,24 @@
    <link rel='icon' type='image/png' sizes='16x16' href='/resources/favicons/favicon-16x16.png'>
    <link rel='manifest' href='/resources/favicons/site.webmanifest'>
    <link rel='mask-icon' href='/resources/favicons/safari-pinned-tab.svg' color='#5bbad5'>
-   <link href='" . (chooseloc('https://fonts.googleapis.com/css?family=Open+Sans', $nointernet) ? 'https://fonts.googleapis.com/css?family=Open+Sans' : $tpdir . '/opensans.css') . "' rel='stylesheet'>
+   <link href='" . ($nointernet ? $tpdir . '/opensans.css' : 'https://fonts.googleapis.com/css?family=Open+Sans') . "' rel='stylesheet'>
    <meta name='msapplication-TileColor' content='#da532c'>
    <meta name='theme-color' content='#ffffff'>
-   <link rel='stylesheet' href='" . (chooseloc('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', $nointernet) ? $final : $tpdir . '/bootstrap.min.css') . "' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
-   <link rel='stylesheet' href='" . (chooseloc('https://bootswatch.com/4/cyborg/bootstrap.min.css', $nointernet) ? $final : $tpdir . '/bootstrap_cyborg.min.css') . "'>
-   <link rel='stylesheet' href='" . (chooseloc('https://cdnjs.cloudflare.com/ajax/libs/octicons/4.4.0/font/octicons.min.css', $nointernet) ? $final : $tpdir . '/octicons.min.css') . "'>
-   <link rel='stylesheet' href='resources/style/mod.css?v=13'>
-   <script src='" . (chooseloc('https://code.jquery.com/jquery-3.3.1.min.js', $nointernet) ? $final : $tpdir . '/jquery-3.3.1.min.js') . "' integrity='sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=' crossorigin='anonymous'></script>
-   <script src='" . (chooseloc('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', $nointernet) ? $final : $tpdir . '/bootstrap.min.js') . "' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
-   <script src='" . (chooseloc('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', $nointernet) ? $final : $tpdir . '/popper.min.js') . "' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
-   <script src='" . (chooseloc('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', $nointernet) ? $final : $tpdir . '/jquery-ui.min.js') . "'></script>
+   <link rel='stylesheet' href='" . ($nointernet ? $tpdir . '/bootstrap.min.css' : 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css') . "' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
+   <link rel='stylesheet' href='" . ($nointernet ? $tpdir . '/bootstrap_cyborg.min.css' : 'https://bootswatch.com/4/cyborg/bootstrap.min.css') . "'>
+   <link rel='stylesheet' href='" . ($nointernet ? $tpdir . '/octicons.min.css' : 'https://cdnjs.cloudflare.com/ajax/libs/octicons/4.4.0/font/octicons.min.css') . "'>
+   <link rel='stylesheet' href='resources/style/mod.css?v=14'>
+   <script src='" . ($nointernet ? $tpdir . '/jquery-3.3.1.min.js' : 'https://code.jquery.com/jquery-3.3.1.min.js') . "' integrity='sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=' crossorigin='anonymous'></script>
+   <script src='" . ($nointernet ? $tpdir . '/bootstrap.min.js' : 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js') . "' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
+   <script src='" . ($nointernet ? $tpdir . '/popper.min.js' : 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js') . "' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
+   <script src='" . ($nointernet ? $tpdir . '/jquery-ui.min.js' : 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js') . "'></script>
   </head>
   ";
  }
  function sendLoader()
  {
   echo "
-   <script src='resources/scripts/extras.js?v=25'></script>
+   <script src='resources/scripts/extras.js?v=26'></script>
   ";
  }
 ?>

c07f998e3d79444016b495efea04a7a339114745
FacuM <facumo.fm@gmail.com>
2018-03-13 22:35:14 -0300

    trigger cache updates

--- head.php
+++ head.php
@@ -34,14 +34,14 @@
    <script src='" . ($nointernet ? $tpdir . '/bootstrap.min.js' : 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js') . "' integrity='sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' crossorigin='anonymous'></script>
    <script src='" . ($nointernet ? $tpdir . '/popper.min.js' : 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js') . "' integrity='sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' crossorigin='anonymous'></script>
    <script src='" . ($nointernet ? $tpdir . '/jquery-ui.min.js' : 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js') . "'></script>
-   <script src='resources/scripts/admin.js?v=2'></script>
+   <script src='resources/scripts/admin.js?v=3'></script>
   </head>
   ";
  }
  function sendLoader()
  {
   echo "
-   <script src='resources/scripts/extras.js?v=27'></script>
+   <script src='resources/scripts/extras.js?v=28'></script>
   ";
  }
 ?>

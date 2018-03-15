16b6bb4185aad2aa23142ebda763a225a92bd527
FacuM <facumo.fm@gmail.com>
2018-03-15 01:00:32 -0300

    fix missing text

--- errors/goback.php
+++ errors/goback.php
@@ -4,7 +4,7 @@
 
 echo '
 
-<p></p>
+<p>' . $LANG['ui_goback_adv'] . '</p>
 <p class="lead">
  <a class="btn btn-primary btn-lg" href="#" onclick="delreload()" role="button">' . $LANG['ui_home'] . '</a>
  <noscript><a class="btn btn-primary btn-lg" href="' . $root . '" role="button">' . $LANG['ui_home'] . '</a></noscript>

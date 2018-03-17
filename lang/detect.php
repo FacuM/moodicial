8fafedf7853710b71183acbebcd0ce08d554efdb
FacuM <facumo.fm@gmail.com>
2018-03-17 00:23:44 -0300

    fix yo mode

--- lang/detect.php
+++ lang/detect.php
@@ -1,63 +1,63 @@
 <?php
- if ($language == 'auto' && !isset($_SESSION['lang']))
- {
+if ($language == 'auto' && !isset($_SESSION['lang']))
+{
   // Automatic language detection code block
   if (isset($_GET['lang']))
   {
-   $lang = $_GET['lang'];
+    $lang = $_GET['lang'];
   }
-   $language = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
-   $language = explode('-', $language[0]);
+  $language = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+  $language = explode('-', $language[0]);
   if (isset($lang))
   {
-   if(session_status() == PHP_SESSION_NONE)
-   {
-    session_start();
-   }
-   $language[0] = $lang;
-   $_SESSION['lang'] = $lang;
+    if(session_status() == PHP_SESSION_NONE)
+    {
+      session_start();
+    }
+    $language[0] = $lang;
+    $_SESSION['lang'] = $lang;
   }
   else
   {
-   if(session_status() == PHP_SESSION_NONE)
-   {
-    session_start();
-   }
-   if (isset($_SESSION['lang']))
-   {
-    $language[0] = $_SESSION['lang'];
-   }
+    if(session_status() == PHP_SESSION_NONE)
+    {
+      session_start();
+    }
+    if (isset($_SESSION['lang']))
+    {
+      $language[0] = $_SESSION['lang'];
+    }
   }
-  if (isset($_GET['yo']))
+  if (!isset($_GET['yo']))
   {
-  if (isset($_GET['lang']) || isset($language[0]))
-  {
-   switch ($language[0]) {
-     case 'en':
-       $language = 'en_US';
-       break;
-     case 'es':
-       $language = 'es_LA';
-       break;
-     case 'pt':
-       $language = 'pt_BR';
-       break;
-     case 'bg':
-       $language = 'bg_BG';
-       break;
-     default:
-       $language = 'en_US';
-       break;
-   }
+    if (isset($_GET['lang']) || isset($language[0]))
+    {
+      switch ($language[0]) {
+        case 'en':
+        $language = 'en_US';
+        break;
+        case 'es':
+        $language = 'es_LA';
+        break;
+        case 'pt':
+        $language = 'pt_BR';
+        break;
+        case 'bg':
+        $language = 'bg_BG';
+        break;
+        default:
+        $language = 'en_US';
+        break;
+      }
+    }
+    else
+    {
+      $language = 'en_US';
+    }
   }
   else
   {
-   $language = 'en_US';
+    $language = 'yo_YO';
   }
-  }
-  else
-  {
-   $language = 'yo_YO';
-  }
- }
+}
 ?>
